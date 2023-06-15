9448af816d27c365e1e3f7038f75e5a7cb7d2799
phamchautuan <113250652+phamchautuan@users.noreply.github.com>
2023-06-15 15:20:13 +0700

    new

--- DoAnCoSo/Controllers/HomeController.cs
+++ DoAnCoSo/Controllers/HomeController.cs
@@ -8,7 +8,6 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.UI;
-
 namespace DoAnCoSo.Controllers
 {
     public class HomeController : Controller
@@ -21,7 +20,7 @@ namespace DoAnCoSo.Controllers
             //
 
 
-            //ViewBag.Keyword = searchString;
+            ViewBag.Keyword = searchString;
             //if (!string.IsNullOrEmpty(searchString)) all_car = (IOrderedQueryable<Car>)all_car.Where(a => a.TenXe.Contains(searchString));
 
 
@@ -38,14 +37,26 @@ namespace DoAnCoSo.Controllers
            ViewBag.size = items;
             ViewBag.currentSize = size;
             if (page == null) page = 1;
-           // var all_car = (from Car in db.SanPhams select Car).OrderBy(m => m.MaXe);
-
-            var all_car = from car in db.SanPhams select car;
+            var all_car = (from Car in db.SanPhams select Car).OrderBy(m => m.MaXe);
+            if (!string.IsNullOrEmpty(searchString)) all_car = (IOrderedQueryable<SanPham>)all_car.Where(a => a.TenXe.Contains(searchString));
+           // var all_car = from car in db.SanPhams select car;
             int PageSize = (size ?? 6);
             int pageNum = page ?? 1;
             return View(all_car.ToPagedList(pageNum, PageSize));
-        }
 
+         
+        }
+        //public ActionResult SapXepTheoTen(int? page, string searchString)
+        //{
+        //    ViewBag.Keyword = searchString;
+
+        //    if (page == null) page = 1;
+        //    var all_car = (from Car in db.SanPhams select Car).OrderBy(m => m.MaXe);
+        //    if (!string.IsNullOrEmpty(searchString)) all_car = (IOrderedQueryable<SanPham>)all_car.Where(a => a.TenXe.Contains(searchString));
+        //    int pageSize = 3;
+        //    int pageNum = page ?? 1;
+        //    return View(all_car.ToPagedList(pageNum, pageSize));
+        //}
         public ActionResult Detail(int id)
         {
             var D_xe = db.SanPhams.Where(m => m.MaXe == id).First();
@@ -54,14 +65,11 @@ namespace DoAnCoSo.Controllers
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
-
             return View();
         }
-
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
